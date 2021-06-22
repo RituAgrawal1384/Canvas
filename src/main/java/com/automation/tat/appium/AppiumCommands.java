831f6a9874d354a5abc79019941686fd303459a6
Ritu Agrawal <ritu.agrawal@prudential.com.sg>
2021-06-22 14:32:03 +0800

    pom update

--- src/main/java/com/automation/tat/appium/AppiumCommands.java
+++ src/main/java/com/automation/tat/appium/AppiumCommands.java
@@ -9,7 +9,6 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.ios.IOSDriver;
@@ -17,17 +16,7 @@ import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -825,7 +814,8 @@ public class AppiumCommands {
             }
         }
 
-        ((AndroidDriver) driver).pressKeyCode(AndroidKeyCode.BACK);
+        ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
+
     }
 
 
